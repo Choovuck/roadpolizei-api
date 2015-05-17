2335de7547bcde78eb5f72663ad2008c527b5df0
choovuck <nickname.qxo@gmail.com>
2015-05-17 16:53:31 +0300

    restored missing variable

--- api/reports/reports.controller.js
+++ api/reports/reports.controller.js
@@ -123,10 +123,6 @@ exports.exportById = function(req, res) {
           });
         });
         report.files = files;
-        fileStates.on('downloaded', function() {
-          res.status(200).json(report);
-        });
-        fileStates.rp.download();
       } else {
         res.status(404);
       }
